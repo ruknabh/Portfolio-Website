bfb57c45243185cd10b9d593c7e6de07e3ce9314
Ruknabh <ruknabhbhattacharyya009@gmail.com>
2026-01-23 20:00:25 +0530

    chore: clean Next.js + Tailwind v4 baseline (md working)

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500 md:bg-green-500">
+      <h1 className="text-white text-4xl font-bold underline">
+        Tailwind v4 works
+      </h1>
+    </div>
+  );
+}
